eec9080370b616d65e80054ce9e7494e753a72b8
qgan99 <37915115+qgan99@users.noreply.github.com>
2023-04-24 11:46:31 -0500

    Add files via upload

--- prog.py
+++ prog.py
@@ -1,15 +1,17 @@
 import serial
+import csv
 
-ser1 = serial.Serial('/dev/ttyAMA1')
-ser.baudrate = 115200
+ser1 = serial.Serial('/dev/ttyAMA1', 115200)
+print(ser1.name)
+with open('milestone2.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    for row in csv_reader:
+        angle = row[0]
+        speed = row[1]
+        duration = row[2]
+        command_str = "angle: " + angle + " speed: " + speed + " duration: " + duration + "\n"
+        print(command_str)
+        #ser1.write(command_str)
+        ser1.write(bytes(command_str))
 
-file = open('milestone2.csv')
-
-for line in file:
-  num  = line.split()
-  speed = num[1]
-  angle = num[0]
-  duration = num[2]
-  command_str = "angle: " + angle + "speed: " + speed + "duration: " + duration
-  ser1.write(bytes(command_str))
 ser1.close()
